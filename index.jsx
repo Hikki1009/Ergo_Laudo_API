7684180ab14fbf9f3c37a2a63ae20a5d2f5f6e3b
Hikki1009 <rafaelgdsfisio@gmail.com>
2026-07-17 19:19:06 -0300

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppLaudo from './ErgoLaudo_v2_FINAL';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AppLaudo />
+  </React.StrictMode>
+);
